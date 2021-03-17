f2556620a9ec3f541ec2686a03719cbae41ec972
Mohammad Choudhury <mohammadchoudhury@users.noreply.github.com>
2021-03-17 02:05:04 +0000

    Created VideoDetail component to display youtube video and title

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
 	state = { videos: [], selectedVideo: null };
@@ -19,7 +20,11 @@ class App extends React.Component {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
-				<VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					videos={this.state.videos}
+					onVideoSelect={this.onVideoSelect}
+				/>
 			</div>
 		);
 	}
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+	if (!video) {
+		return <div>Loading...</div>;
+	}
+
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+	return (
+		<div>
+			<div className="ui embed">
+				<iframe
+					title="Video Player"
+					src={videoSrc}
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowFullScreen
+				/>
+			</div>
+			<div className="ui segment">
+				<h4 className="ui header">{video.snippet.title}</h4>
+				<p>{video.snippet.description}</p>
+			</div>
+		</div>
+	);
+};
+
+export default VideoDetail;
